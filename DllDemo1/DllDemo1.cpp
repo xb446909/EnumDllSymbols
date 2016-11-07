e31628c2e5a0e32201ee603475e8089525552a54
ByronXiong <xiong-bin@live.com>
2016-11-07 23:24:47 +0800

    添加了DllDemo1和DllDemo2项目

---
+++ DllDemo1/DllDemo1.cpp
@@ -0,0 +1,13 @@
+// DllDemo1.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "DllDemo1.h"
+
+
+// 这是导出函数的一个示例。
+DLLDEMO1_API int fnDllDemo1(void)
+{
+    return 42;
+}
+
---
+++ DllDemo2/DllDemo2.cpp
@@ -0,0 +1,13 @@
+// DllDemo2.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "DllDemo2.h"
+
+
+// 这是导出函数的一个示例。
+DLLDEMO2_API int fnDllDemo2(void)
+{
+    return 42;
+}
+
